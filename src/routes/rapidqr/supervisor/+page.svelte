740bdcd510c8bdeb8774e5f40b0e34499313569d
birdpump <birdpump@gmail.com>
2024-10-04 14:51:05 -0700

    update rapid qr logic

--- src/routes/rapidqr/supervisor/+page.svelte
+++ src/routes/rapidqr/supervisor/+page.svelte
@@ -1,29 +1,31 @@
 <script>
     import { io } from "socket.io-client";
-    import {onMount} from "svelte";
+    import QRCode from 'qrcode';
+    import { onMount } from "svelte";
 
     let socket;
+    let canvasElement;
 
-    onMount((async) => {
+    onMount(() => {
         socket = io("http://localhost:3000");
 
         socket.on("connect", () => {
-
-            console.log(socket.id); // x8WIv7-mJelg7on_ALbx
+            console.log(socket.id);
         });
 
         socket.on("disconnect", () => {
-            console.log(socket.id); // undefined
+            console.log(socket.id);
         });
 
         socket.on("message", (data) => {
             console.log(data);
+            QRCode.toCanvas(canvasElement, data, { errorCorrectionLevel: 'M' }, function (error) {
+                if (error) console.error(error);
+                console.log('QR code generated successfully!');
+            });
         });
     });
 
-    function emit(){
-        socket.emit('message', `hello people`);
-    }
 </script>
 
-<button on:click={emit}>Send hello to people</button>
+<canvas bind:this={canvasElement}></canvas>
