e3a84a00452f7f1b40fc2c351f8ee3c7d34a1237
birdpump <birdpump@gmail.com>
2024-10-08 12:10:20 -0700

    update axios instance

--- src/lib/api/config/axios.js
+++ src/lib/api/config/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
     baseURL: 'http://localhost:3000', // Your base URL
-    timeout: 10000,
+    timeout: 5000,
 });
 
 export default axiosInstance;
