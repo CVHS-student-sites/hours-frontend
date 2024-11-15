fdd5879d83e0a8f02cfab22a20127aeae4413ded
birdpump <birdpump@gmail.com>
2024-11-15 11:45:05 -0800

    update server side fetch logic

--- src/routes/(app)/+layout.server.js
+++ src/routes/(app)/+layout.server.js
@@ -1,25 +1,23 @@
-import { error } from '@sveltejs/kit';
+import {error} from '@sveltejs/kit';
 
 export async function load({cookies, url}) {
     const auth = cookies.get('token');
 
-    let response;
     try {
-        response = await fetch(`${url.origin}/api/auth/check-auth`, {
+        let response = await fetch(`${url.origin}/api/auth/check-auth`, {
             credentials: 'include',
             headers: {
                 'Authorization': `Bearer ${auth}`,
             },
         });
+
+        return {
+            response: await response.json()
+        };
     } catch (err) {
         console.error("Error getting user profile:", err);
         error(503, {
             message: 'API Origin Server Offline'
         });
     }
-
-
-    return {
-        response: await response.json()
-    };
 }
